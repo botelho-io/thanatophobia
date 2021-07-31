ec916357f665249271a42bf7f5746ccefd8ca6c6
Dmitry Porunov <hi@dmitry.email>
2021-07-31 13:25:42 +0300

    Ah ah ah! You'll never understand why this one works!

--- extension.js
+++ extension.js
@@ -31,6 +31,8 @@ const PanelMenu = imports.ui.panelMenu;
 const _ = Gettext.gettext;
 const {GObject, St, Clutter} = imports.gi;
 
+const GLib = imports.gi.GLib;
+let sourceId = null;
 
 const Indicator = GObject.registerClass(
     class Indicator extends PanelMenu.Button {
@@ -66,7 +68,7 @@ const Indicator = GObject.registerClass(
                 this._timeout = null;
             }
 
-            this._timeout = Mainloop.timeout_add(50, () => this._refresh());
+            sourceId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 50, () => this._refresh());
         }
     });
 
@@ -81,11 +83,10 @@ class Extension {
     }
 
     disable() {
-        if (this._timeout) {
-            Mainloop.source_remove(this._timeout);
-            this._timeout = null;
+        if (sourceId) {
+            GLib.Source.remove(sourceId);
+            sourceId = null;
         }
-
         this._indicator.destroy();
         this._indicator = null;
     }
