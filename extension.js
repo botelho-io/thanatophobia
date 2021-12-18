3a71256c82ac7f051614f5ec11036ea24b8464de
keyoted <keyoted@gmail.com>
2021-12-18 20:03:00 +0000

    Improved the age calculation algorithm. The previous algorithm used a rough estimation of the number of ms between birthdays instead of actually calculating it, this gave the final calculation an error of about 0.0011yrs (aprox. 9.6hrs) wich is significant when asking the user for their birthday in minutes.

--- extension.js
+++ extension.js
@@ -26,13 +26,16 @@ const Indicator = GObject.registerClass(class Indicator extends PanelMenu.Button
         super._init(0.0, _('Current Age Indicator'));
 
         // Init variables
-        this.year = NaN
-        this.month = NaN
-        this.day = NaN
-        this.hour = NaN
-        this.minute = NaN
-        this.expectancy = NaN
-        this.bDate = new Date();
+        this.year = NaN;
+        this.month = NaN;
+        this.day = NaN;
+        this.hour = NaN;
+        this.minute = NaN;
+        this.expectancy = NaN;
+        this.birthDate = new Date();
+        this.ms_lastBirthDay = NaN;
+        this.ms_betweenBirthdays = NaN;
+        this.ageYears = NaN;
 
         // Add age indicator label
         this.label = new St.Label({
@@ -64,7 +67,10 @@ const Indicator = GObject.registerClass(class Indicator extends PanelMenu.Button
 
     // Calculates user's age
     _getAge() {
-        return ((Date.now() - this.bDate.getTime()) / 3.15576e+10).toFixed(9);
+        // Calculate the fractional part of the user's age
+        const ageFraction = (Date.now() - this.ms_lastBirthDay) / this.ms_betweenBirthdays;
+        // The user's age is the integer part of the date + the fractional part
+        return (this.ageYears + ageFraction).toFixed(9);
     }
 
     // Reads data from settings file
@@ -77,7 +83,28 @@ const Indicator = GObject.registerClass(class Indicator extends PanelMenu.Button
         this.minute = this.gsettings.get_int(GS_KEY_MINUTE);
         this.expectancy = this.gsettings.get_double(GS_KEY_LIFE_EXPECTANCY);
         // Calculate user's birth date
-        this.bDate = new Date(this.year, this.month - 1, this.day, this.hour, this.minute);
+        this.birthDate = new Date(this.year, this.month - 1, this.day, this.hour, this.minute);
+        // TODO: Test the algorithms robustness for people born February 29
+        // Set-up variables for getting the user's exact age
+        // Find user's birthday on the current year
+        const today = new Date();
+        const currentYear = today.getFullYear();
+        const ms_birthDayOnCurrentYear = new Date(currentYear, this.month - 1, this.day, this.hour).getTime();
+        // Find user's next and last birthday
+        let ms_nextBirthDay = NaN
+        if (today.getTime() > ms_birthDayOnCurrentYear) {
+            // User's birthday has passed
+            this.ms_lastBirthDay = ms_birthDayOnCurrentYear;
+            ms_nextBirthDay = new Date(currentYear + 1, this.month - 1, this.day, this.hour).getTime();
+        } else {
+            // User's birthday has not yet passed
+            this.ms_lastBirthDay = new Date(currentYear - 1, this.month - 1, this.day, this.hour).getTime();
+            ms_nextBirthDay = ms_birthDayOnCurrentYear;
+        }
+        // Find how many ms will elapse between the user's last and next birthdays
+        this.ms_betweenBirthdays = ms_nextBirthDay - this.ms_lastBirthDay;
+        // Calculate the integer part of the user's age
+        this.ageYears = Math.abs(new Date(Date.now() - this.birthDate.getTime()).getFullYear() - 1970);
         // Recalculate users life expectancy
         this.menuItem.label.set_text(_(`${(this._getAge() / this.expectancy * 100).toFixed(4).toString()}% of global average life expectancy`));
     }
